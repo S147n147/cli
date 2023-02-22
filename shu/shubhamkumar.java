31ecebb24fb328372e433f156bfd7d5fbb78c0a6
shubham <nilamwarshubham147@gmail.com>
2023-02-22 20:10:08 +0530

    2nd commit

---
+++ shu/shubhamkumar.java
@@ -0,0 +1,15 @@
+package jav;
+
+public class shu{
+
+	public static void main(String[]args) {
+		
+		//write your code here
+		System.out.println("hello world");
+		int num10 = 6;
+		int num11 = 6;
+		int num12 = 4;
+		int sum = num12 + num10 + num11;
+		System.out.println(sum);
+	}
+}
